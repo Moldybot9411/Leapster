be10de13ed4e1c87231d376284c5dc2826bdb6bb
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2024-09-05 14:39:12 +0200

    Add Level with basic level editor

--- Leapster/Leapster/Game.cs
+++ Leapster/Leapster/Game.cs
@@ -60,6 +60,8 @@ public class Game
     {
         InitRenderer();
 
+        Level level = new();
+
         while(Running)
         {
             RenderLoop();
---
+++ Leapster/Leapster/Level.cs
@@ -0,0 +1,98 @@
+﻿using ImGuiNET;
+using System.Numerics;
+
+namespace Leapster;
+
+public class Level
+{
+    public List<Vector4> Boxes { get; private set; } = new();
+
+    public Vector4 BoxColor = new(0, 0, 0, 1);
+
+    private bool spawnBox;
+    private Vector2 spawnBoxSize;
+
+    private string generatedText = "";
+
+    public Level()
+    {
+        Game.Instance.OnRender += OnRender;
+
+        Boxes.Add(new Vector4(10, 10, 20, 20));
+    }
+
+    private void OnRender()
+    {
+#if DEBUG
+        if (ImGui.Begin("Level Editor"))
+        {
+            ImGui.ColorPicker4("Box color", ref BoxColor);
+
+
+            ImGui.Checkbox("Spawn box (SPACE)", ref spawnBox);
+
+            ImGui.SliderFloat2("Box size", ref spawnBoxSize, 0, 100);
+
+            if (ImGui.Button("Generate level data"))
+            {
+                generatedText = string.Join(",\n", Boxes.Select(box => $"new Vector4({box.X}, {box.Y}, {box.Z}, {box.W})"));
+            }
+
+            ImGui.InputTextMultiline("Generated", ref generatedText, (uint)generatedText.Length + 1, Vector2.Zero);
+
+            if (ImGui.CollapsingHeader("Boxes"))
+            {
+                Vector2 displaySize = ImGui.GetIO().DisplaySize;
+
+                for (int i = 0; i < Boxes.Count; i++)
+                {
+                    if (!ImGui.TreeNode($"Box #{i}"))
+                        continue;
+
+                    bool changed = false;
+
+                    void DoChange(Func<bool> func)
+                    {
+                        if (func())
+                            changed = true;
+                    }
+
+                    Vector4 box = Boxes[i];
+
+                    DoChange(() => ImGui.SliderFloat("X", ref box.X, 0, displaySize.X));
+                    DoChange(() => ImGui.SliderFloat("y", ref box.Y, 0, displaySize.Y));
+                    DoChange(() => ImGui.SliderFloat("Width", ref box.Z, 0, 500));
+                    DoChange(() => ImGui.SliderFloat("Height", ref box.W, 0, 500));
+
+                    if (changed)
+                        Boxes[i] = box;
+
+                    ImGui.TreePop();
+                }
+            }
+
+            ImGui.SetWindowSize(Vector2.Zero, ImGuiCond.Once);
+            ImGui.End();
+        }
+
+        if (ImGui.IsKeyPressed(ImGuiKey.Space, false) && spawnBox)
+        {
+            Vector2 mouseLocation = ImGui.GetIO().MousePos;
+            Vector4 rect = new(mouseLocation.X, mouseLocation.Y, spawnBoxSize.X, spawnBoxSize.Y);
+
+            Boxes.Add(rect);
+        }
+
+        ImDrawListPtr draw = ImGui.GetBackgroundDrawList();
+#endif
+
+        foreach (Vector4 rect in Boxes)
+        {
+            Vector2 topLeft = new Vector2(rect.X, rect.Y);
+            Vector2 topRight = topLeft + new Vector2(rect.Z, rect.W);
+            draw.AddRectFilled(topLeft, topRight, ImGui.ColorConvertFloat4ToU32(BoxColor));
+        }
+
+    }
+
+}
