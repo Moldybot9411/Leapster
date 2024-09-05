bccfb2febf47d511a5c9ce0f4b4244c7855b2952
Moldybot9411 <joewhite8844@gmail.com>
2024-09-05 11:25:57 +0200

    Create Player.cs

---
+++ Leapster/Leapster/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leapster;
+
+public class Player
+{
+    public Rectangle rect { get; set; }
+}
