8fb594a0e12145e7c8a3bb32d0c669b70c4e10e9
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2024-09-06 13:58:04 +0200

    Create Level editor

---
+++ Leapster/Leapster/Screens/LevelEditorScreen.cs
@@ -0,0 +1,41 @@
+﻿using ImGuiNET;
+using System.Numerics;
+
+namespace Leapster.Screens;
+
+public class LevelEditorScreen : Screen
+{
+    private Level dummyLevel;
+
+    public override void Show()
+    {
+        dummyLevel = new();
+    }
+
+    public override void Hide()
+    {
+    }
+
+    private void RenderLevelEditorWindow()
+    {
+        if (!ImGui.Begin("Level Editor"))
+        {
+            ImGui.End();
+            return;
+        }
+
+        ImGui.SetWindowSize(Vector2.Zero);
+
+        ImGui.End();
+    }
+
+    private Vector4 box;
+
+    public override void RenderImGui()
+    {
+        RenderLevelEditorWindow();
+
+        //dummyLevel.OnRender();
+    }
+
+}
