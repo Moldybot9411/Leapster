4e507f1de1909c1f720202886c309c1710da51e6
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2024-09-07 11:29:18 +0200

    Add H to player character

--- Leapster/Leapster/Player.cs
+++ Leapster/Leapster/Player.cs
@@ -27,7 +27,17 @@ public class Player
 
         Vector2 topLeft = position;
         Vector2 bottomRight = topLeft + size;
-        ImGui.GetBackgroundDrawList().AddRectFilled(topLeft + Screenshake.ShakeOffset, bottomRight + Screenshake.ShakeOffset, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 0, 0, 1)));
+		Vector2 center = (topLeft + bottomRight) * 0.5f;
+
+		ImDrawListPtr draw = ImGui.GetBackgroundDrawList();
+		draw.AddRectFilled(topLeft + Screenshake.ShakeOffset, bottomRight + Screenshake.ShakeOffset, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 0, 0, 1)));
+
+		// Draw the centered "H"
+		ImGui.PushFont(Game.Instance.BigFont); // Replace 'yourFont' with the font you want to use
+		Vector2 textSize = ImGui.CalcTextSize("H");
+		Vector2 textPosition = center - textSize * 0.5f + Screenshake.ShakeOffset;
+		draw.AddText(textPosition, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, 1)), "H");
+		ImGui.PopFont();
     }
 
     private void UpdatePosition()
