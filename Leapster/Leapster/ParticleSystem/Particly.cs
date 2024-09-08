e55cd0c436bcf751d12dc7fc6be355948445da98
Moldybot9411 <joewhite8844@gmail.com>
2024-09-08 18:30:21 +0200

    First visibility of P A R T I C L E S

--- Leapster/Leapster/ParticleSystem/Particly.cs
+++ Leapster/Leapster/ParticleSystem/Particly.cs
@@ -6,22 +6,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http.Headers;
+using System.Security.Cryptography;
 
 namespace Leapster.ParticleSystem;
 
 public class Particly
 {
-    public int Amount = 10;
-    public float InitialVelocityStrength = 2.0f;
+    public int Amount = 100;
+    public float InitialVelocityStrength = 50.0f;
     public Vector2 Position = new(50, 50);
     public Vector4 Color = new(1, 0, 0, 1);
     public float StartSize = 10.0f;
 
-    private List<Particle> existingParticles;
-    private Random rand;
+    private List<Particle> existingParticles = new List<Particle>();
 
     public Particly()
     {
+        Start();
         Game.Instance.GameScreen.OnRender += Render;
     }
 
@@ -29,14 +30,24 @@ public class Particly
     {
         for(int i = 0; i < Amount; i++)
         {
-            Vector2 startVelocity = new Vector2((float)rand.NextDouble() * InitialVelocityStrength, (float)rand.NextDouble() * InitialVelocityStrength);
-            existingParticles.Add(new Particle(Color, startVelocity, StartSize));
+            Vector2 startVelocity = new Vector2(GetRandomNumber(-1.0f, 1.0f) * InitialVelocityStrength, GetRandomNumber(-1.0f, 1.0f) * InitialVelocityStrength);
+            existingParticles.Add(new Particle(Position, Color, startVelocity, StartSize));
         }
     }
 
     private void Render()
     {
-        ImGui.GetBackgroundDrawList().AddCircle(new Vector2(50, 50), 10f, ImGui.ColorConvertFloat4ToU32(new Vector4(0.8f, 0.8f, 0.8f, 1)));
+        foreach(Particle particle in existingParticles)
+        {
+            particle.UpdatePosition();
+            ImGui.GetBackgroundDrawList().AddCircleFilled(particle.AbsolutePosition, particle.Size, ImGui.ColorConvertFloat4ToU32(particle.Color));
+        }
+    }
+
+    private float GetRandomNumber(float minimum, float maximum)
+    {
+        Random random = new Random();
+        return (float)random.NextDouble() * (maximum - minimum) + minimum;
     }
 }
 
@@ -46,18 +57,20 @@ public class Particle
     public Vector2 AbsolutePosition;
     public bool WorldSpace = false;
     public float Size;
+    public float LifeTime;
     public Vector4 Color;
-    public Vector2 velocity;
+    public Vector2 Velocity;
 
-    public Particle(Vector4 color, Vector2 initialVelocity, float size)
+    public Particle(Vector2 startPosition, Vector4 color, Vector2 initialVelocity, float size)
     {
+        AbsolutePosition = startPosition;
         Color = color;
-        velocity = initialVelocity;
+        Velocity = initialVelocity;
         Size = size;
     }
 
     public void UpdatePosition()
     {
-        Console.WriteLine("YURRRRR");
+        AbsolutePosition += Velocity * ImGui.GetIO().DeltaTime;
     }
 }
