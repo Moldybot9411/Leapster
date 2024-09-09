6ef26ba3326d80b9dcab2e51c6a761ad8bebe226
Weiss Jonas (PEA6-Fe-FI) <joewhite8844@gmail.com>
2024-09-09 15:13:07 +0200

    Added Level Select screen

---
+++ Leapster/Leapster/Screens/LevelSelectScreen.cs
@@ -0,0 +1,154 @@
+﻿using ImGuiNET;
+using Leapster.LevelEditor;
+using NativeFileDialogSharp;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leapster.Screens;
+
+public class LevelSelectScreen : IScreen
+{
+    private Vector2 childSize = Vector2.Zero;
+    private string levelsFolder = "";
+    private string[] levelPaths = [];
+    private List<string> levelNames = [];
+
+    public unsafe void Show()
+    {
+        Game.Instance.clearColor = Color.FromArgb(255, 21, 10, 8);
+    }
+
+    public void Hide()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RenderImGui()
+    {
+        ImGui.Begin("LevelSelect",
+            ImGuiWindowFlags.NoMove |
+            ImGuiWindowFlags.NoTitleBar |
+            ImGuiWindowFlags.NoResize |
+            ImGuiWindowFlags.NoCollapse |
+            ImGuiWindowFlags.NoBackground |
+            ImGuiWindowFlags.NoDocking |
+            ImGuiWindowFlags.NoSavedSettings);
+
+        Vector2 parentSize = ImGui.GetWindowSize();
+
+        ImGui.SetNextWindowPos((parentSize - childSize) / 2);
+
+        ImGui.PushStyleVar(ImGuiStyleVar.FrameRounding, 2);
+        ImGui.PushFont(Game.Instance.BigFont);
+
+        ImGui.PushStyleColor(ImGuiCol.Text, Color.FromArgb(255, 247, 239, 238).ToImGuiColor());
+
+        ImGui.PushStyleColor(ImGuiCol.Button, Color.FromArgb(255, 225, 67, 44).ToImGuiColor());
+        ImGui.PushStyleColor(ImGuiCol.ButtonHovered, Color.FromArgb(255, 255, 100, 44).ToImGuiColor());
+        ImGui.PushStyleColor(ImGuiCol.ButtonActive, Color.FromArgb(255, 133, 42, 29).ToImGuiColor());
+
+        if (ImGui.Button(FontAwesome6.FolderOpen))
+        {
+            string currentPath = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName;
+            DialogResult result = Dialog.FolderPicker(currentPath);
+
+            if (result.IsOk)
+            {
+                levelsFolder = result.Path;
+            }
+        }
+        ImGui.SetItemTooltip("Open Levels folder");
+
+        ImGui.SameLine();
+        ImGui.InputText("Levels folder", ref levelsFolder, 1000);
+
+        ImGui.SameLine();
+        if (ImGui.Button(FontAwesome6.Spinner))
+        {
+            RescanLevelFolder();
+        }
+        ImGui.SetItemTooltip("Rescan Folder");
+
+        if (ImGui.BeginTable("Levels", 3, ImGuiTableFlags.Borders | ImGuiTableFlags.Resizable))
+        {
+            ImGui.TableSetupColumn("Name");
+            ImGui.TableSetupColumn("Date of creation");
+            ImGui.TableSetupColumn("##bruh", ImGuiTableColumnFlags.None, 0.1f);
+
+            ImGui.TableHeadersRow();
+
+            for (int i = 0; i < levelNames.Count(); i++)
+            {
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+
+                //ImGui.Text($"H {h}");
+                ImGui.Text(levelNames[i].ToString());
+
+                ImGui.TableNextColumn();
+
+                ImGui.TableNextColumn();
+
+                ImGui.Button("Play");
+            }
+
+            ImGui.EndTable();
+        }
+
+        if (ImGui.BeginPopupModal("ERROR", ImGuiWindowFlags.NoResize))
+        {
+            ImGui.Text("Specified level path is not valid!");
+
+            if (ImGui.Button("Close"))
+            {
+                ImGui.CloseCurrentPopup();
+            }
+
+            ImGui.EndPopup();
+        }
+
+        ImGui.PopFont();
+
+        ImGui.PopStyleVar();
+
+        ImGui.SetWindowSize(ImGui.GetIO().DisplaySize);
+        ImGui.SetWindowPos(Vector2.Zero);
+
+        ImGui.End();
+    }
+
+    private void RescanLevelFolder()
+    {
+        if (!Directory.Exists(levelsFolder))
+        {
+            Console.WriteLine("Specified level path is not valid!");
+            ImGui.OpenPopup("ERROR");
+            return;
+        }
+        levelNames.Clear();
+
+        string[] files = Directory.GetFiles(levelsFolder);
+
+        levelPaths = files;
+
+        foreach (string file in files)
+        {
+            try
+            {
+                EditorLevel level = JsonConvert.DeserializeObject<EditorLevel>(File.ReadAllText(file));
+                levelNames.Add(level.Name);
+            } catch(Exception)
+            {
+                // Ignored, just dont display level no one cares
+            }
+        }
+    }
+
+}
