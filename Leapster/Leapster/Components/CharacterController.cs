053b3bb65b4873dff616ef92e5b04756d5496984
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2024-09-09 17:33:21 +0200

    Small Refactoring

--- Leapster/Leapster/Components/CharacterController.cs
+++ Leapster/Leapster/Components/CharacterController.cs
@@ -1,13 +1,7 @@
 ﻿using ImGuiNET;
 using Leapster.ObjectSystem;
-using Silk.NET.SDL;
-using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Leapster.Components;
 
