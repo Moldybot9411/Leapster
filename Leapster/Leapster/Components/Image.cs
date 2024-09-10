7f88f7085851bf3556ffae21adcf85a089c62598
Moldybot9411 <joewhite8844@gmail.com>
2024-09-10 13:00:24 +0200

    add Image

---
+++ Leapster/Leapster/Components/Image.cs
@@ -0,0 +1,33 @@
+﻿using ImGuiNET;
+using Leapster.ObjectSystem;
+using System.Numerics;
+
+namespace Leapster.Components;
+
+internal class Image : Component
+{
+    private string path;
+
+    private IntPtr textureId;
+
+    public Image(string pathToImage)
+    {
+        path = pathToImage;
+    }
+
+    public override void Start()
+    {
+        textureId = ImGuiExtensions.LoadImage(path, out _);
+        
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        Vector2 topLeft = new(AssignedObject.Rect.X, AssignedObject.Rect.Y);
+        Vector2 bottomRight = new(AssignedObject.Rect.X + AssignedObject.Rect.Width, AssignedObject.Rect.Y + AssignedObject.Rect.Height);
+
+        ImGui.GetBackgroundDrawList().AddImage(textureId, topLeft + Screenshake.ShakeOffset, bottomRight + Screenshake.ShakeOffset);
+    }
+
+}
